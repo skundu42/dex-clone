36e2822810109b270fe16627299258d9a024faaf
skundu42 <skundu_be19@thapar.edu>
2024-01-31 22:18:44 +0530

    initial

---
+++ hardhat.config.js
@@ -0,0 +1,48 @@
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: {
+//     compilers: [
+//       {
+//         version: "0.7.6",
+//         settings: {
+//           evmVersion: "istanbul",
+//           optimizer: {
+//             enabled: true,
+//             runs: 1000,
+//           },
+//         },
+//       },
+//     ],
+//   },
+//   networks: {
+//     hardhat: {
+//       forking: {
+//         url: "your",
+//       },
+//     },
+//   },
+// };
+
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 5000,
+        details: { yul: false },
+      },
+    },
+  },
+  networks: {
+    
+    shm: {
+        url: "https://sphinx.shardeum.org/",
+        accounts: [``],
+  },
+}
+};
